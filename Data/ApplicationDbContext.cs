7eef39119eb7696162525a757467dc2daeb9fa17
bozhidarv <bozh.bv03@gmail.com>
2020-05-04 09:10:42 +0300

    Adding basic CRUD operations for the User table and linking the db with the app

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PollApi.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PollApi.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
