1c46ca193cfb7db19a87f755296ef027d17ef407
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-03-21 08:50:06 +0200

    fix: middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/bozhidarv/poll-api/internal/models"
+	"github.com/bozhidarv/poll-api/internal/services"
+)
+
+func AuthMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		unauthorizedError := &models.ApiError{
+			Code:    401,
+			Message: "Unauthorized",
+		}
+
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			services.HandleError(unauthorizedError, &w)
+			return
+		}
+
+		token := strings.Split(authHeader, " ")[1]
+
+		userId, expTimestamp := services.ParseJwtToken(token)
+
+		if expTimestamp < float64(time.Now().Unix()) {
+			services.HandleError(unauthorizedError, &w)
+			return
+		}
+
+		newCtx := context.WithValue(r.Context(), "userId", userId)
+
+		// validate the token
+		// if the token is invalid, return an unauthorized error
+		// if the token is valid, continue to the next middleware
+
+		// handle the request
+		handler.ServeHTTP(w, r.WithContext(newCtx))
+	})
+}
